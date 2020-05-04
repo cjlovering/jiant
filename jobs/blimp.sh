9c70ea7132b55d3f04091acc6f3ee5c325eb61ab
Charlie Lovering <cjlovering@wpi.edu>
2020-05-04 16:41:00 -0400

    original blimp job

---
+++ jobs/blimp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#$ -cwd
+#$ -l short
+#$ -l gpus=2
+#$ -e ./logs/
+#$ -o ./logs/
+
+mkdir -p ./logs/
+. ~/.bashrc
+conda activate jiant
+. ./user_config.sh;
+
+# full sentence evaluation
+python main.py --config_file jiant/config/blimp/blimp_gpt2.conf --overrides="exp_name=blimp-gpt2,run_name=simplelm,target_tasks=blimp-simpleLM,input_module=gpt2-large"
