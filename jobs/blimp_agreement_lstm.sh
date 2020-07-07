f41555268874360d17aa9ba1fb32bafb4c1f9462
cjlovering <cjlovering@wpi.edu>
2020-07-07 15:56:07 -0400

    frozen

--- jobs/blimp_agreement_lstm.sh
+++ jobs/blimp_agreement_lstm.sh
@@ -15,20 +15,20 @@ conda activate jiant
 # full sentence evaluation
 if [ "$SGE_TASK_ID" -eq 1 ]
 then
-python main.py --config_file jiant/config/blimp/blimp_lstm_finetune.conf --overrides="exp_name=blimp-lstm,run_name=distractor_agreement_relational_noun_probing-0.5-good,target_tasks=distractor_agreement_relational_noun_probing-0.5-good"
+python main.py --config_file jiant/config/blimp/blimp_lstm_finetune.conf --overrides="exp_name=blimp-lstm-frozen,run_name=distractor_agreement_relational_noun_probing-0.5-good,target_tasks=distractor_agreement_relational_noun_probing-0.5-good"
 fi
 
 if [ "$SGE_TASK_ID" -eq 2 ]
 then
-python main.py --config_file jiant/config/blimp/blimp_lstm_finetune.conf --overrides="exp_name=blimp-lstm,run_name=distractor_agreement_relational_noun_probing-0.5-bad,target_tasks=distractor_agreement_relational_noun_probing-0.5-bad"
+python main.py --config_file jiant/config/blimp/blimp_lstm_finetune.conf --overrides="exp_name=blimp-lstm-frozen,run_name=distractor_agreement_relational_noun_probing-0.5-bad,target_tasks=distractor_agreement_relational_noun_probing-0.5-bad"
 fi
 
 if [ "$SGE_TASK_ID" -eq 3 ]
 then
-python main.py --config_file jiant/config/blimp/blimp_lstm_finetune.conf --overrides="exp_name=blimp-lstm,run_name=distractor_agreement_relational_noun_probing-1.0-good,target_tasks=distractor_agreement_relational_noun_probing-1.0-good"
+python main.py --config_file jiant/config/blimp/blimp_lstm_finetune.conf --overrides="exp_name=blimp-lstm-frozen,run_name=distractor_agreement_relational_noun_probing-1.0-good,target_tasks=distractor_agreement_relational_noun_probing-1.0-good"
 fi
 
 if [ "$SGE_TASK_ID" -eq 4 ]
 then
-python main.py --config_file jiant/config/blimp/blimp_lstm_finetune.conf --overrides="exp_name=blimp-lstm,run_name=distractor_agreement_relational_noun_probing-1.0-bad,target_tasks=distractor_agreement_relational_noun_probing-1.0-bad"
+python main.py --config_file jiant/config/blimp/blimp_lstm_finetune.conf --overrides="exp_name=blimp-lstm-frozen,run_name=distractor_agreement_relational_noun_probing-1.0-bad,target_tasks=distractor_agreement_relational_noun_probing-1.0-bad"
 fi
