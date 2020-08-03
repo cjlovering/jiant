03b559be1f30f7733cda5ca6ecf687e3ceedb112
cjlovering <cjlovering@wpi.edu>
2020-08-03 10:46:54 -0400

    doc

--- proc/process_finetune.py
+++ proc/process_finetune.py
@@ -8,25 +8,23 @@ import sklearn.metrics
 @plac.opt("folder", "folder name")
 @plac.opt("rate", "co occurence rate", choices=["0", "1", "5"])
 def main(prop="gap", rate="0", folder="2020-08-02"):
-
+    # Load initial data with additional meta information.
     with jsonlines.open(f"../data_gen/{prop}/{prop}_test.jsonl") as reader:
         data = [obj for obj in reader]
 
-    name = f"./{folder}/{prop}/{rate}/{prop}_finetune_{rate}_test.tsv"
-    df = pd.read_csv(name, sep="\t")
-    df.head()
-
+    # Prepare results.
+    df = pd.read_csv(f"./{folder}/{prop}/{rate}/{prop}_finetune_{rate}_test.tsv", sep="\t")
     df["sentence"] = df["sentence_1"].apply(untokenize)
     new_cols = df.apply(lambda x: extract_meta(x, data), axis=1, result_type="expand")
     df = pd.concat([df, new_cols], axis="columns")
     df["error"] = df["label"] != df["true_label"]
     df["bad"] = ((df.section == "both") | (df.section == "bad-only")).astype(int)
-
     both = df[df.section == "both"]
     neither = df[df.section == "neither"]
     # good = df[df.section == "good-only"]
     bad = df[df.section == "bad-only"]
 
+    # Print results.
     score = lambda x: x["error"].mean()
     I_pred_true = sklearn.metrics.mutual_info_score(df["true_label"], df["prediction"])
     I_pred_dist = sklearn.metrics.mutual_info_score(df["bad"], df["prediction"])
